3a0a76c895a6e0cff08f475f37cd3d9eb1c69f3b
Hasan Khan <76878543+hasan1301@users.noreply.github.com>
2021-06-21 03:29:25 +0600

    add file

---
+++ class.js
@@ -0,0 +1,11 @@
+class student {
+    constructor(sId, sName) {
+        this.id = sId;
+        this.name = sName;
+        this.school = 'Khan Acadamy'
+    }
+}
+
+let student1 = new student(1, 'Hasib')
+
+console.log(student1);
---
+++ threedot.js
@@ -0,0 +1,7 @@
+let age = [10, 13, 45, 15]
+let age2 = [5, 8, 9]
+let age3 = [10, 20, 30]
+
+let allAges = [...age, ...age2, ...age3]
+
+console.log(allAges);
